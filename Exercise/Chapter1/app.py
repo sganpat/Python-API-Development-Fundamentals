cf4febefd56719392aeb093b890194d6bffffb5e
sganpat <sganpat@gmail.com>
2020-09-22 21:21:50 -0400

    Finished Chapter 1 basic exercises

---
+++ Exercise/Chapter1/app.py
@@ -0,0 +1,92 @@
+from flask import Flask, jsonify, request
+from http import HTTPStatus
+
+
+app = Flask(__name__)
+
+recipes = [
+    {
+        'id':1,
+        'name':'Egg Salad',
+        'description':'This is a lovely egg salad recipe.'
+    },
+    {
+        'id':2,
+        'name':'Tomato pasta',
+        'description':'This is a lovely tomato pasta.'
+    }
+]
+
+@app.route('/recipes',methods=['GET'])
+
+def get_recipes():
+    return jsonify({'data':recipes})
+
+
+@app.route('/recipes/<int:recipe_id>',methods=['GET'])
+
+def get_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
+
+    if recipe:
+        return jsonify(recipe)
+    
+    return jsonify({'message':'recipe not found'}), HTTPStatus.NOT_FOUND
+
+
+@app.route('/recipes',methods=['POST'])
+
+def create_recipe():
+    data = request.get_json()
+    name = data.get('name')
+    description = data.get('description')
+
+    recipe = {
+        'id':len(recipes)+1,
+        'name':name,
+        'description':description
+    }
+
+    recipes.append(recipe)
+
+    return jsonify(recipe), HTTPStatus.CREATED
+
+
+@app.route('/recipes/<int:recipe_id>',methods=['PUT'])
+
+def update_recipe(recipe_id):
+
+    recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
+
+    if not recipe:
+        return jsonify({'message':'recipe not found.'}), HTTPStatus.NOT_FOUND
+
+    data = request.get_json()
+
+    recipe.update(
+
+        {
+            'name':data.get('name'),
+            'description':data.get('description')
+        }
+    )
+
+    return jsonify(recipe)
+
+@app.route('/recipes/<int:recipe_id>',methods=['DELETE'])
+
+def delete_recipe(recipe_id):
+
+    recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
+
+    if not recipe:
+        return jsonify({'message':'recipe not found.'}), HTTPStatus.NOT_FOUND
+
+    recipes.pop(recipes.index(recipe))
+
+    return jsonify({'message':'recipe deleted', 'recipe':recipe})
+
+if __name__ == '__main__':
+    app.run()
+
+ 
